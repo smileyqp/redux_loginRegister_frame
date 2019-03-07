d467546509b9ad6bb9e3962519189aa03ca649fc
smileyqp <468417415@qq.com>
2019-03-07 11:59:05 +0800

    this is react login and register frontend frame

---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import thunk from 'redux-thunk';
+
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import {createStore,applyMiddleware} from 'redux';
+import rootReducers from './reducers';
+
+import {Provider} from 'react-redux';
+
+
+const store = createStore(
+    rootReducers,
+    composeWithDevTools(
+        applyMiddleware(thunk,logger),
+    )
+);
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
